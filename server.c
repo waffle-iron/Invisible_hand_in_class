2e70c57f8b31fe0456d3ee0f8f97692dad396d42
sohnji-ss <strawbellbe@gmail.com>
2017-03-31 09:59:51 +0900

    server

---
+++ server.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("socket()\n");
+	printf("connect()\n");
+	printf("send()\n");
+	printf("recv()\n");
+	printf("closesocket()\n");
+
+	return 0;
+
+}
