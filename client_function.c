94b85699aa3275c94e39c4fe6b79cab4b8f6676a
JHLee <loveljhs2@gmail.com>
2017-03-31 10:02:37 +0900

    skeleton code in fucntion

---
+++ client_function.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("클라이언트 센드시 체크함수구현\n");
+
+	return 0;
+}
+
---
+++ server_function.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("서버 리시버시 체크하는 함수구현\n");
+	return 0;
+}
