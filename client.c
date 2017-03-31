a11529d9931ece4d5ee08106f6827be18cc8edd6
Kujin <dktmzdf@gmail.com>
2017-03-31 10:29:12 +0900

    1

--- client.c
+++ client.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
+#include "function.h"
+
+#define PORT 22// 바꿔야됨
+#define BUFSIZE 1024
 
 int main(){
 	
-	printf("socket()\n connect()\n send()\n recv()\n closesocket()");
+	printf("socket():먼저 소켓(포트 등)을 연다.");
+	printf("connect()\n:서버와 연결한다."); 
+	printf("send()\n:보낼때 파일이름 및 파일 내용이 맞는지 확인하고 보낸다.");
+	printf("recv()\n:보낸 파일이 맞는지 확인한다.");
+	printf("closesocket():소켓을 닫는다");
+	client_function();
 	return 0;
 }
