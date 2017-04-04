774929cf7427577e0d4b138573e3d27b978f78d6
kujin <p93006@hanmail.net>
2017-04-04 23:39:28 +0900

    testgit

--- client.c
+++ client.c
@@ -12,24 +12,11 @@ int main(){
 	//unsigned int count;
 	
 
-	printf("socket():먼저 소켓(포트 등)을 연다.");
-	printf("connect():서버와 연결한다.\n"); 
-	printf("send():보낼때 파일이름 및 파일 내용이 맞는지 확인하고 보낸다.\n");
-	printf("recv():보낸 파일이 맞는지 확인한다.\n");
-	printf("closesocket():소켓을 닫는다");
+	printf("socket():\n");
+	printf("connect():\n"); 
+	printf("send():\n");
+	printf("recv():\n");
+	printf("closesocket():");
 	client_function();
-	//fd = socket(AF_UNIX,SOCK_STREAM,0);
-	//if(listen_sock == INVALID_SOCKET) err_quit("socket()");
-	//addr.sin_family = AF_INET;
-	//add.sin_addr.S_un.S_addr = inet_addr(IP);
-	//addr.sin_port =htons(PORT);
-	//if(connect(s,(struc sockaddr *)&addr, sizeof(addr) ==  SDOCKET_ERROR){
-	//	perror("connect : ");
-	//	printf("fail to connect.\n);
-	//	closesocket(s);
-	//	return 1;
-	//}
-	
-	
-	return 0;
+	return 0;	
 }
