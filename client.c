1b06199be14882848bad22ce21f59c2768a4e3d9
kujin <p93006@hanmail.net>
2017-04-05 00:09:13 +0900

    client modif

--- client.c
+++ client.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<netdb.h>
 #include<string.h>
 #include "function.h"
 
-#define PORT 22//¼öÁ¤ÇØ¾ßµÊ.
-#define BUFSIZE 1024
-#define IP "127.0.0.1"
-
-int main(){
-	//SOCKADDR_IN addr;
-	//unsigned int count;
+#define PORTNUM 9005
+int main(void){
 	
 
 	printf("socket():\n");
@@ -18,5 +14,31 @@ int main(){
 	printf("recv():\n");
 	printf("closesocket():");
 	client_function();
-	return 0;	
+	int sd, n;
+	char buf[256];
+	struct sockaddr_in sin;
+  
+	if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        	perror("socket");
+	        exit(1);
+      	}
+  
+      	memset((char *)&sin, '\0', sizeof(sin));
+      	sin.sin_family = AF_INET;
+      	sin.sin_port = htons(PORTNUM);
+    	sin.sin_addr.s_addr = inet_addr("192.168.162.133");
+  
+      	strcpy(buf, "I am a client.");
+      	if (sendto(sd, buf, strlen(buf)+1, 0,
+               (struct sockaddr *)&sin, sizeof(sin)) == -1) {
+        	perror("sendto");
+        	exit(1);
+	}
+	n = recvfrom(sd, buf, 255, 0, NULL, NULL);
+     	buf[n] = '\0';
+      	printf("** From Server : %s\n", buf);
+  
+      	return 0;
+
+	
 }
