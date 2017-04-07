624d5adec561404768119d9ff6f3072ffcc363d4
sohnji-ss <strawbellbe@gmail.com>
2017-04-07 10:22:49 +0900

    server fork

--- server.c
+++ server.c
@@ -34,21 +34,43 @@ int main()
 	if (bind(sd, (struct sockaddr *)&sin, sizeof(sin))) {
 		perror("bind");
 		exit(1);
-		}
+	}
+
+
+	///어디로 넣을지
+	////클라이언트: 파일 이름 크기 체크 하고 보낸다 - 파일 크면 나눠서보낸다
+	//서버 : 내가 가지고 있는 하드용량 -  
+	
 	while (1) {
 		if ((recvfrom(sd, buf, 255, 0,
-						(struct sockaddr *)&cli, &clientlen)) == -1) {
+			(struct sockaddr *)&cli, &clientlen)) == -1) {
 			perror("recvfrom");
 			exit(1);
-			}
+		}
+
 		printf("** From Client : %s\n", buf);
 		strcpy(buf, "Hello Client");
+		
 		if ((sendto(sd, buf, strlen(buf)+1, 0,
-						(struct sockaddr *)&cli, sizeof(cli))) == -1) {
+			(struct sockaddr *)&cli, sizeof(cli))) == -1) {
 			perror("sendto");
 			exit(1);
-			}
 		}
+
+//받는 응용프로그램 만들기 - 자식을 처리할수있는 알고리즘?
+		switch (fork()) {
+			case 0 :
+				prinf("** Fork Client \n");
+				close(sd);
+				dup2(ns, STDIN_FILENO);
+				dup2(ns, STDOUT_FILENO);
+				close(ns);
+				execl("./han", "han", (char *)0);
+		}
+		close(ns);
+
+
+	}
 
 
 
