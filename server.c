e7a6104f8b49362d55f7f4bd1b2af98abf8e97dc
sohnji-ss <strawbellbe@gmail.com>
2017-04-10 23:57:46 +0900

    server fork and address change

--- server.c
+++ server.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <netdb.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "function.h"
 
-#define PROTUM 9001
+#define PORTNUM 9005
 
 int main()
 {
@@ -16,10 +20,11 @@ int main()
 	printf("recv() : 파일을 받는다.\n");
 	printf("closesocket() : 소켓을 닫는다. \n");
 
-	
+
 	char buf[256];
 	struct sockaddr_in sin, cli;
 	int sd, clientlen = sizeof(cli);
+
 
 	if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
 		perror("socket");
@@ -29,48 +34,82 @@ int main()
 	memset((char *)&sin, '\0', sizeof(sin));
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(PORTNUM);
-	sin.sin_addr.s_addr = inet_addr("192.168.162.133");
+	//sin.sin_addr.s_addr = inet_addr("192.168.162.133");
+	sin.sin_addr.s_addr = htonls(INADDR_ANY);
 
 	if (bind(sd, (struct sockaddr *)&sin, sizeof(sin))) {
 		perror("bind");
 		exit(1);
 	}
 
+/*
+while (1) {
+    printf("\nUDP Server: waiting for connection...");
+    bytes_read = recvfrom(socket_fd, recv_data, 1023, 0,
+            (struct sockaddr *) &client_addr, &sin_size);
 
-	///어디로 넣을지
-	////클라이언트: 파일 이름 크기 체크 하고 보낸다 - 파일 크면 나눠서보낸다
-	//서버 : 내가 가지고 있는 하드용량 -  
-	
+    if (bytes_read > 0) {
+                // a connection has been established
+            recv_data[bytes_read] = '\0';
+                printf("\nUDP Server: received -> %s", recv_data);
+            pid = fork();
+
+            if (pid < 0) {
+                perror("UDP Server: ERROR while forking new process.\n");
+                exit(1);
+            }
+            // check if the process ID is zero
+            if (pid == 0) {
+                // we are now inside the new forked process
+                char result[50];
+                int len = sprintf(result, "%d", server_parse_command(recv_data));
+                len = sendto(socket_fd, result, len, 0,
+                (struct sockaddr *) &client_addr, sin_size);
+                close(socket_fd);
+                exit(0);
+            }
+    }
+}
+*/
+	int pid;
 	while (1) {
-		if ((recvfrom(sd, buf, 255, 0,
-			(struct sockaddr *)&cli, &clientlen)) == -1) {
+		int bytes_read = (recvfrom(sd, buf, 255, 0,(struct sockaddr *)&cli, &clientlen));
+		if (bytes_read == -1) {
 			perror("recvfrom");
 			exit(1);
 		}
 
 		printf("** From Client : %s\n", buf);
 		strcpy(buf, "Hello Client");
-		
-		if ((sendto(sd, buf, strlen(buf)+1, 0,
-			(struct sockaddr *)&cli, sizeof(cli))) == -1) {
-			perror("sendto");
-			exit(1);
-		}
-
-//받는 응용프로그램 만들기 - 자식을 처리할수있는 알고리즘?
-		switch (fork()) {
-			case 0 :
-				prinf("** Fork Client \n");
-				close(sd);
-				dup2(ns, STDIN_FILENO);
-				dup2(ns, STDOUT_FILENO);
-				close(ns);
-				execl("./han", "han", (char *)0);
-		}
-		close(ns);
 
 
+		if (bytes_read > 0) {
+							 // a connection has been established
+					 buf[bytes_read] = '\0';
+					 printf("\nUDP Server: received -> %s", buf);
+
+					 pid = fork();
+
+					 if (pid < 0) {
+                	perror("UDP Server: ERROR while forking new process.\n");
+                	exit(1);
+    				}
+            // check if the process ID is zero
+			    if (pid == 0) {
+			                // we are now inside the new forked process
+			    				char result[50];
+			            int len = sprintf(result, "%d", server_parse_command(buf));
+									len = sendto(sd, buf, strlen(buf)+1, 0,(struct sockaddr *)&cli, sizeof(cli));
+			            //len = sendto(socket_fd, result, len, 0,(struct sockaddr *) &client_addr, sin_size);
+									if (len == -1) {
+										perror("sendto");
+										exit(1);
+									}
+									close(sd);
+			            exit(0);
+			    }
 	}
+}
 
 //파일 이름만 아니면 경로만 보낼 것인지
 //클라이언트 - 이름과 용량과 아이피
