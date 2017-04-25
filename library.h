8b7bdd4b84f4e1ee0f8cd46e60a3c19af1417441
dktmzdf <dktmzdf@gmail.com>
2017-04-25 19:23:55 +0900

    Timer merge final

--- library.h
+++ library.h
@@ -19,8 +19,6 @@
 
 #define SIZEBUF 1024
 #define PORTNUM 9000
-<<<<<<< HEAD
-
 void UdpClient(int argc, char** argv, int sd, struct sockaddr_in sin);
 void UdpServer(int sd, struct sockaddr_in cli);
 void UdpFileTrans(int sd, struct sockaddr_in sin, socklen_t add_len, char* file_name);
@@ -32,8 +30,6 @@ void TcpFileTrans(int sd, char* file_name);
 void TcpDirTrans(int sd, char* dir_name);
 
 
-typedef struct _file_information{	// 파일 디렉토리 저장하는 구조체
-=======
 void UdpClient(int argc, char** argv, int sd,	struct sockaddr_in sin);
 void UdpServer();
 void TcpClient(int argc, char** argv);
@@ -43,7 +39,7 @@ long long File_Size(char *file_name);
 double File_Transfer_Speed(double total_time,char *F);
 double File_Transfer_Timer(long start_tv_sec,long start_tv_usec,long end_tv_sec,long end_tv_usec);
 typedef struct _file_information{	//����ü
->>>>>>> Timer
+
 	struct dirent dent;
 	char or_file_dir;
 }file_information;
